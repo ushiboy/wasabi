ac549168e10b0ce5466d7b6a4e482145efa023ec
ushiboy <ushiboy.dev@gmail.com>
2022-04-28 22:33:35 +0900

    added useCount hook

---
+++ src/presentations/pages/Greeting/hooks/useCount.ts
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export const useCount = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount((count) => count + 1);
+
+  return {
+    count,
+    increment,
+  };
+};
