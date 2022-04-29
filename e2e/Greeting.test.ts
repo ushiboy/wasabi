77c920292e250fe1ae86866bc710fccaeb02847d
ushiboy <ushiboy.dev@gmail.com>
2022-04-29 15:20:28 +0900

    fix test code

--- e2e/Greeting.test.ts
+++ e2e/Greeting.test.ts
@@ -1,22 +1,20 @@
 import { test, expect } from "@playwright/test";
+import { GreetingPage } from "./pageObjects";
 
 test.describe("Greeting Page", () => {
   test("should increase in count each time the button is pressed", async ({
     page,
   }) => {
-    // Go to http://localhost:3000/
-    await page.goto("http://localhost:3000/");
+    const p = new GreetingPage(page);
+    await p.goto();
 
-    // Click text=count is: 0
-    await page.locator("[data-hook=counter]").click();
-    await expect(page.locator("[data-hook=counter]")).toHaveText("count is: 1");
+    await p.increaseCount();
+    await expect(p.counter).toHaveText("count is: 1");
 
-    // Click text=count is: 1
-    await page.locator("[data-hook=counter]").click();
-    await expect(page.locator("[data-hook=counter]")).toHaveText("count is: 2");
+    await p.increaseCount();
+    await expect(p.counter).toHaveText("count is: 2");
 
-    // Click text=count is: 2
-    await page.locator("[data-hook=counter]").click();
-    await expect(page.locator("[data-hook=counter]")).toHaveText("count is: 3");
+    await p.increaseCount();
+    await expect(p.counter).toHaveText("count is: 3");
   });
 });
